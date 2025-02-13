da94e182a4767a3e77b8424662932ea38ab5c4ee
unknown <dainiusrainys99@gmail.com>
2025-02-13 10:46:52 +0200

    created function activeBtn which onclick determines active button and sets color mode according to that. Also added media queries and changed color of active button class

--- js/main.js
+++ js/main.js
@@ -2,8 +2,14 @@ const slider = document.querySelector('#sizeSlider');
 const sizeValue = document.querySelector('#sizeValue');
 const board = document.querySelector('#grid');
 const colorPicker = document.querySelector('#colorPicker');
+
+// All buttons
 const clearBtn = document.querySelector('#clearBtn');
+const colorBtn = document.querySelector('#colorBtn');
 const rainbowBtn = document.querySelector('#rainbowBtn');
+const ereaserBtn = document.querySelector('#ereaserBtn');
+
+const buttons = [colorBtn, rainbowBtn, ereaserBtn];
 
 
 //Grid resizing
@@ -12,13 +18,17 @@ let resize = () => {
     board.innerHTML = '';
     let size = slider.value;
     for (let i =0 ; i < size*size; i++){
-        let div = document.createElement('div');
-        div.setAttribute("class", "grid-element");
-        board.appendChild(div);
+        createDiv();
     }
 
     board.setAttribute("style", `grid-template-columns: repeat(${size}, 1fr); grid-template-rows: repeat(${size}, 1fr)`);
     sizeValue.textContent = `${size} X ${size}`;
+}
+
+let createDiv = () => {
+    let div = document.createElement('div');
+    div.setAttribute("class", "grid-element");
+    board.appendChild(div);
 }
 
 ////-----------------Possible way------------------------------
@@ -47,10 +57,6 @@ let changeColor = (e, color) => {
     let item = e.target;
     if(item.className != "grid"){
     item.setAttribute("style", `background-color: ${color}`);
-    console.log(item);
-    }
-    else{
-        console.log('you clicked grid');
     }
 }
 
@@ -79,19 +85,42 @@ let generateColor = () =>{
     }
     return `#${randomColor}`;
 }
+
+let ereaser = () => {
+    let color = "none";
+    return color;
+}
+
+// Using array of buttons and event property set active class for currently clicked button
+
+let activeBtn = (e) => {
+    let btn = e.target.id;
+    buttons.forEach((button) =>{
+        if(button.id != btn){
+            button.setAttribute("class", "");
+        }
+        else{
+            button.setAttribute("class", "active");
+        }
+    })
+}
     
 
 //Event Listeners
 
 slider.addEventListener('input', resize);
 
-rainbowBtn.addEventListener("click", () => {
-    rainbowBtn.setAttribute("class", "active");
-});
+buttons.forEach((button) => {
+    button.addEventListener("click", activeBtn);
+})
+
 
 board.addEventListener("click", (e) => {
     if(rainbowBtn.classList.contains('active')){
         changeColor(e, generateColor());
+    }
+    else if(ereaserBtn.classList.contains('active')){
+        changeColor(e, ereaser());
     }
     else {
         changeColor(e, getColor());
